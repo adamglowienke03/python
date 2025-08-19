ec215862db44c3c6464b13e52a8ccc298ccd061a
AdameX7 <glowienke_adam@wp.pl>
2025-08-19 18:23:46 +0200

    Update calculator.py

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -17,21 +17,21 @@ operations = {
     "/": divide,
 }
 
-first_number = float(input("What is your first number? "))
-print("+\n-\n*\n/")
-operation = input("Pick an operation: ")
-second_number = float(input("What is your second number? "))
-result = round(operations[operation](first_number, second_number), 2)
-print(f"{first_number} {operation} {second_number} = {result}")
+def calculator():
+    first_number = float(input("What is your first number? "))
 
-while True:
-    is_continue = input("Is it continue with a result? (y/n): ")
-    if is_continue == "y":
-        print("+\n-\n*\n/")
+    while True:
+        for key in operations:
+            print(key)
         operation = input("Pick an operation: ")
         second_number = float(input("What is your next number? "))
-        new_result = round(operations[operation](result, second_number), 2)
-        print(f"{result} {operation} {second_number} = {new_result}")
-        result = new_result
-    else:
-        break
+        result = round(operations[operation](first_number, second_number), 2)
+        print(f"{first_number} {operation} {second_number} = {result}")
+        is_continue = input(f"Is it continue with a {result}? (y/n): ")
+        if is_continue == "y":
+            first_number = result
+        else:
+            print("\n" * 10)
+            calculator()
+
+calculator()
